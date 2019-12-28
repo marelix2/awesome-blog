364fa9cfb107f42f4284662f6ed6a2f5f915e74f
Michal Rusinek <marelix2@gmail.com>
2019-12-28 18:31:35 +0100

    Updates

--- src/views/ArticlePage/ArticlePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Markdown from 'react-markdown'
-
-const ArticlePage = ({source}) => (
-   <Markdown source={source} />
-  );
-
-
-export default ArticlePage;
--- src/views/MainPage/utils.js
+++
@@ -1,7 +0,0 @@
-export const getArticle = (src, setNewArticle) => {
-  import(/* webpackMode: "eager" */`./../../articles/${src}.md`).then(newOne => {
-    fetch(newOne.default)
-      .then(response => response.text())
-      .then(text => setNewArticle(text))
-  })
-}
